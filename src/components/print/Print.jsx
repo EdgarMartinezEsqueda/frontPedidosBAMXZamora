4c3075aced74dfd8db14f7594d19e6670f734988
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-10-25 00:16:13 -0600

    style: improved dark mode

--- src/components/print/Print.jsx
+++ src/components/print/Print.jsx
@@ -6,7 +6,7 @@ const Print = ({ contentRef }) => {
 
   return (
     <section className="w-full flex justify-center items-center p-4">
-      <button onClick={reactToPrintFn} className="flex items-center gap-2 px-5 py-2.5 border-2 border-gray-700 text-gray-700 rounded-lg hover:bg-gray-700 hover:text-white transition-all duration-200 font-medium" >
+      <button onClick={reactToPrintFn} className="flex items-center gap-2 px-5 py-2.5 border-2 border-gray-700 dark:bg-gray-600 text-gray-700 dark:text-gray-400 rounded-lg hover:bg-gray-700 hover:text-white transition-all duration-200 font-medium" >
         <FiPrinter className="text-lg" />
       </button>
     </section>
